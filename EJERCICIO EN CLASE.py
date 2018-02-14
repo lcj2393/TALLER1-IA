6ba37fe07ee7dec9eb0c920f09a9297e54f57fdd
Luis Jordan <lc.jordan93@gmail.com>
2018-02-13 23:06:51 -0500

    CORRECCIONES FINALES Y ENTREGA

---
+++ EJERCICIO EN CLASE.py
@@ -0,0 +1,14 @@
+#-*-coding:utf-8 -*-
+import random
+
+numero_aleatorio =
+
+Acertado = True
+while Acertado:
+    numero = int(input("Ingrese un Valor"))
+
+    if numero > numero_aleatorio:
+        pass
+
+    def run():
+        pass
